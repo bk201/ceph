3f7ac1808f8296d19e7518de8cc4c6299953268e
Kiefer Chang <kiefer.chang@suse.com>
2019-10-14 17:42:03 +0800

    fix preview modal unit test

--- src/pybind/mgr/dashboard/frontend/src/app/ceph/cluster/osd/osd-creation-preview-modal/osd-creation-preview-modal.component.spec.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/ceph/cluster/osd/osd-creation-preview-modal/osd-creation-preview-modal.component.spec.ts
@@ -1,5 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { BsModalRef } from 'ngx-bootstrap/modal';
+import { i18nProviders } from '../../../../../testing/unit-test-helper';
+import { SharedModule } from '../../../../shared/shared.module';
+import { DriveGroup } from '../osd-form/osd-form-data';
 import { OsdCreationPreviewModalComponent } from './osd-creation-preview-modal.component';
 
 describe('OsdCreationPreviewModalComponent', () => {
@@ -8,6 +15,8 @@ describe('OsdCreationPreviewModalComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule, ReactiveFormsModule, SharedModule, RouterTestingModule],
+      providers: [BsModalRef, i18nProviders],
       declarations: [OsdCreationPreviewModalComponent]
     }).compileComponents();
   }));
@@ -15,6 +24,7 @@ describe('OsdCreationPreviewModalComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(OsdCreationPreviewModalComponent);
     component = fixture.componentInstance;
+    component.driveGroup = new DriveGroup();
     fixture.detectChanges();
   });
 
