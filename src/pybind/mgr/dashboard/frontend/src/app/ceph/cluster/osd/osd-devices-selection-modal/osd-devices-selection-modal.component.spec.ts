116b80715a6679abcfb71e9adaeeec1e73a875e8
Kiefer Chang <kiefer.chang@suse.com>
2019-10-17 12:25:18 +0800

    Add frontend unit tests
    Signed-off-by: Kiefer Chang <kiefer.chang@suse.com>

--- src/pybind/mgr/dashboard/frontend/src/app/ceph/cluster/osd/osd-devices-selection-modal/osd-devices-selection-modal.component.spec.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/ceph/cluster/osd/osd-devices-selection-modal/osd-devices-selection-modal.component.spec.ts
@@ -1,32 +1,83 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
 import { BsModalRef } from 'ngx-bootstrap/modal';
-import { i18nProviders } from '../../../../../testing/unit-test-helper';
+import { configureTestBed, i18nProviders } from '../../../../../testing/unit-test-helper';
 import { SharedModule } from '../../../../shared/shared.module';
+import { InventoryDeviceFiltersChangeEvent } from '../../inventory/inventory-devices/inventory-device-filters-change-event.interface';
+import { InventoryDevice } from '../../inventory/inventory-devices/inventory-device.model';
 import { InventoryDevicesComponent } from '../../inventory/inventory-devices/inventory-devices.component';
 import { OsdDevicesSelectionModalComponent } from './osd-devices-selection-modal.component';
 
 describe('OsdDevicesSelectionModalComponent', () => {
   let component: OsdDevicesSelectionModalComponent;
   let fixture: ComponentFixture<OsdDevicesSelectionModalComponent>;
+  const devices: InventoryDevice[] = [
+    {
+      hostname: 'node0',
+      uid: '1',
+      blank: false,
+      type: 'SSD',
+      id: '/dev/sda',
+      size: 1024,
+      rotates: false,
+      available: true,
+      dev_id: '',
+      extended: undefined,
+      vendor: 'AAA',
+      model: 'aaa'
+    }
+  ];
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [FormsModule, SharedModule, ReactiveFormsModule, RouterTestingModule],
-      providers: [BsModalRef, i18nProviders],
-      declarations: [OsdDevicesSelectionModalComponent, InventoryDevicesComponent]
-    }).compileComponents();
-  }));
+  const expectSubmitButton = (enabled: boolean) => {
+    const nativeElement = fixture.debugElement.nativeElement;
+    const button = nativeElement.querySelector('.modal-footer button');
+    expect(button.disabled).toBe(!enabled);
+  };
+
+  configureTestBed({
+    imports: [FormsModule, SharedModule, ReactiveFormsModule, RouterTestingModule],
+    providers: [BsModalRef, i18nProviders],
+    declarations: [OsdDevicesSelectionModalComponent, InventoryDevicesComponent]
+  });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(OsdDevicesSelectionModalComponent);
     component = fixture.componentInstance;
+    component.devices = devices;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should disable submit button initially', () => {
+    expectSubmitButton(false);
+  });
+
+  it('should enable submit button after filtering some devices', () => {
+    const event: InventoryDeviceFiltersChangeEvent = {
+      filters: [
+        {
+          label: 'hostname',
+          prop: 'hostname',
+          value: 'node0',
+          formatValue: 'node0'
+        },
+        {
+          label: 'size',
+          prop: 'size',
+          value: '1024',
+          formatValue: '1KiB'
+        }
+      ],
+      filterInDevices: devices,
+      filterOutDevices: []
+    };
+    component.onFilterChange(event);
+    fixture.detectChanges();
+    expectSubmitButton(true);
   });
 });
